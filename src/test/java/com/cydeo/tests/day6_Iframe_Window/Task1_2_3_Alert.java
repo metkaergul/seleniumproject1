b9b58f3e4b300a989dcee0bc094d6cea8ca46481
metkaergul <metkaergul@gmail.com>
2022-09-02 15:29:58 +0300

    alert tasks is created

---
+++ src/test/java/com/cydeo/tests/day6_Iframe_Window/Task1_2_3_Alert.java
@@ -0,0 +1,28 @@
+package com.cydeo.tests.day6_Iframe_Window;
+
+public class Task1_2_3_Alert {
+
+
+
+}
+/*TC #1: Information alert practice
+1. Open browser
+2. Go to website: http://practice.cydeo.com/javascript_alerts
+3. Click to “Click for JS Alert” button
+4. Click to OK button from the alert
+5. Verify “You successfully clicked an alert” text is displayed.
+
+TC #2: Confirmation alert practice
+1. Open browser
+2. Go to website: http://practice.cydeo.com/javascript_alerts
+3. Click to “Click for JS Confirm” button
+4. Click to OK button from the alert
+5. Verify “You clicked: Ok” text is displayed.
+
+TC #3: Information alert practice
+1. Open browser
+2. Go to website: http://practice.cydeo.com/javascript_alerts
+3. Click to “Click for JS Prompt” button
+4. Send “hello” text to alert
+5. Click to OK button from the alert
+6. Verify “You entered:  hello” text is displayed.  */
