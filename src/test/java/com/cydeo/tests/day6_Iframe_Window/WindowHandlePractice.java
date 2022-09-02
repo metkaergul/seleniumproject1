408a949ea1b6796dfa5b31b585124f9cb1dcc138
metkaergul <metkaergul@gmail.com>
2022-09-02 16:21:08 +0300

    windowhandle practice created

---
+++ src/test/java/com/cydeo/tests/day6_Iframe_Window/WindowHandlePractice.java
@@ -0,0 +1,16 @@
+package com.cydeo.tests.day6_Iframe_Window;
+
+public class WindowHandlePractice {
+
+
+
+}
+/*TC #5: Window Handle practice
+
+1. Create a new class called: T5_WindowsPractice
+2. Create new test and make set ups
+3. Go to : https://practice.cydeo.com/windows
+4. Assert: Title is “Windows”
+5. Click to: “Click Here” link
+6. Switch to new Window.
+7. Assert: Title is “New Window”   */
