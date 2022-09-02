b4ffe2ec7a8149b6f5e75c047a276079d6b747e8
metkaergul <metkaergul@gmail.com>
2022-09-02 15:56:56 +0300

    alert tasks is done

--- src/test/java/com/cydeo/tests/day6_Iframe_Window/Task1_2_3_Alert.java
+++ src/test/java/com/cydeo/tests/day6_Iframe_Window/Task1_2_3_Alert.java
@@ -1,6 +1,77 @@
 package com.cydeo.tests.day6_Iframe_Window;
 
+import com.cydeo.tests.Utilities;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class Task1_2_3_Alert {
+
+    WebDriver driver;
+    @BeforeClass
+    public void setupMethod(){
+
+        WebDriverManager.chromedriver().setup();
+        driver = Utilities.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        // https://practice.cydeo.com/dropdown
+        driver.get(" http://practice.cydeo.com/javascript_alerts");
+
+    }
+    @Test
+    public void informationAlert() throws InterruptedException {
+
+
+        WebElement clickforJSAlertButton = driver.findElement(By.xpath("//button[.='Click for JS Alert']"));
+        clickforJSAlertButton.click();
+        Thread.sleep(2000);
+
+        Alert alert= driver.switchTo().alert();
+        alert.accept();
+
+
+
+
+    }
+    @Test
+    public void confirmationAlert() throws InterruptedException {
+
+        WebElement clickforJSConfirmButton = driver.findElement(By.xpath("//button[.='Click for JS Confirm']"));
+        clickforJSConfirmButton.click();
+        Thread.sleep(2000);
+
+        Alert alert = driver.switchTo().alert();
+
+        alert.dismiss();
+
+    }
+    @Test
+    public void promptAlert() throws InterruptedException {
+
+        WebElement clickForJsPromptButton= driver.findElement(By.xpath("//button[.='Click for JS Prompt']"));
+        clickForJsPromptButton.click();
+
+        Thread.sleep(2000);
+
+        Alert alert =driver.switchTo().alert();
+
+        alert.sendKeys("Hello");
+        alert.accept();
+
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
 
 
 
